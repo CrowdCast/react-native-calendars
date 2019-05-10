c15cbf99e31521ae79509ef479772d539a550890
Henry Kung <henrykung@live.ca>
2019-05-10 18:20:26 +0900

    fixed to include filler height styling

--- src/calendar/day/period/style.js
+++ src/calendar/day/period/style.js
@@ -1,49 +1,49 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 import * as defaultStyle from '../../../style';
 
 const STYLESHEET_ID = 'stylesheet.day.period';
+export default function styleConstructor(theme = {}) {
+  const appStyle = { ...defaultStyle, ...theme };
+  const FILLER_HEIGHT = appStyle.baseDayHeight || 34;
 
-const FILLER_HEIGHT = 34;
-
-export default function styleConstructor(theme={}) {
-  const appStyle = {...defaultStyle, ...theme};
   return StyleSheet.create({
     wrapper: {
       alignItems: 'center',
       alignSelf: 'stretch',
-      marginLeft: -1
+      justifyContent: 'center',
+      marginLeft: -1,
     },
     base: {
       //borderWidth: 1,
       width: appStyle.baseDayWidth || 38,
-      height: appStyle.baseDayHeight || FILLER_HEIGHT,
-      alignItems: 'center'
+      height: FILLER_HEIGHT,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     fillers: {
       position: 'absolute',
       height: FILLER_HEIGHT,
       flexDirection: 'row',
       left: 0,
-      right: 0
+      right: 0,
     },
     leftFiller: {
       height: FILLER_HEIGHT,
-      flex: 1
+      flex: 1,
     },
     rightFiller: {
       height: FILLER_HEIGHT,
-      flex: 1
+      flex: 1,
     },
     text: {
-      marginTop: 7,
       fontSize: appStyle.textDayFontSize,
       fontFamily: appStyle.textDayFontFamily,
       fontWeight: '300',
       color: appStyle.dayTextColor || '#2d4150',
-      backgroundColor: 'rgba(255, 255, 255, 0)'
+      backgroundColor: 'rgba(255, 255, 255, 0)',
     },
     today: {
-      backgroundColor: appStyle.todayBackgroundColor
+      backgroundColor: appStyle.todayBackgroundColor,
     },
     todayText: {
       fontWeight: '500',
@@ -51,26 +51,26 @@ export default function styleConstructor(theme={}) {
       //color: appStyle.textLinkColor
     },
     disabledText: {
-      color: appStyle.textDisabledColor
+      color: appStyle.textDisabledColor,
     },
     quickAction: {
       backgroundColor: 'white',
       borderWidth: 1,
-      borderColor: '#c1e4fe'
+      borderColor: '#c1e4fe',
     },
     quickActionText: {
       marginTop: 6,
-      color: appStyle.textColor
+      color: appStyle.textColor,
     },
     firstQuickAction: {
-      backgroundColor: appStyle.textLinkColor
+      backgroundColor: appStyle.textLinkColor,
     },
     firstQuickActionText: {
-      color: 'white'
+      color: 'white',
     },
     naText: {
-      color: '#b6c1cd'
+      color: '#b6c1cd',
     },
-    ...(theme[STYLESHEET_ID] || {})
+    ...(theme[STYLESHEET_ID] || {}),
   });
 }
